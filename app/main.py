5f0c5280377e17b034ec6c2a349d7de3606516d0
Dong Bin Choi <dbc@MacBook-Pro.local>
2026-08-27 13:02:15 +0900

    Implement GET todos endpoint, Fix bugs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from fastapi import fastapi
+from fastapi import FastAPI
 
 app = FastAPI(
         title = "Team Todo API",
         version = "0.1.0"
         )
 
+todos = [
+        {"id" : 1, "title": "Learn GitHub", "completed": False},
+        {"id" : 2, "title": "Build Todo API", "completed": False},
+        ]
+
+
 @app.get("/")
 def root():
     return {"message": "Team Todo API"}
+
+@app.get("/todos")
+def get_todos():
+    return todos
