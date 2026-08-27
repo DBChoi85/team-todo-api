505db1228ddf9ca77b03880eecb438d451f99474
Dong Bin Choi <dbc@MacBook-Pro.local>
2026-08-27 13:15:27 +0900

    Define Todo response model

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(
         title = "Team Todo API",
         version = "0.1.0"
         )
 
+class Todo(BaseModel):
+    id: int
+    title: str
+    comleted: bool
+
 todos = [
-        {"id" : 1, "title": "Learn GitHub", "completed": False},
-        {"id" : 2, "title": "Build Todo API", "completed": False},
+        Todo(id=1, title="Learn Github", completed=False),
+        Todo(id=2, title="Build Todo API", completed=Fase),
         ]
 
 
